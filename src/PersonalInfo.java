01cda6985a36df78acab25ed920aec38431d0bb0
JesusGarcia86 <the_sampritas@hotmail.com>
2021-04-15 08:14:39 -0700

    first commit

---
+++ src/PersonalInfo.java
@@ -0,0 +1,21 @@
+
+public class PersonalInfo {
+
+	public static void main(String[] args) {
+		
+		String name = "Roger Sampritas";
+		String address = "1234 Main St";
+		String city = "Los Angeles";
+		String state = "California";
+		int zipcode = 90019;
+		String phone = "111-000-2244";
+		String college = "Psychology";
+		
+		System.out.println("Name: " + name + "\nAddress: " + address + "\nCity: " + city
+							+ "\nState: " + state + "\nZipCode: " + zipcode + "\nPhone Number: "
+							+ phone + "\nCollege Major: " + college);
+				
+
+	}
+
+}
